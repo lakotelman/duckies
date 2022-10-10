b9d1ee89f10681501d841008227182ee672f4a71
lakotelman <la.n.kot@pm.me>
2022-10-10 13:14:11 -0800

    reconfigure drawers

--- src/components/OptionsDrawer/Drawer.jsx
+++ src/components/OptionsDrawer/Drawer.jsx
@@ -1,8 +1,7 @@
 import ButtonOption from "./ButtonOption";
 const Drawer = (props) => {
   return (
-    <div className="mb-4 collapse collapse-arrow bg-slate-500 h-min rounded-box ">
-      <input type="checkbox" />
+    <div tabIndex={0} className="mb-4 collapse collapse-arrow bg-slate-500 h-min rounded-box ">
       <div className="collapse-title text-xl font-medium">{props.title}</div>
       <div className="collapse-content grid grid-cols-5">
         {props.optionsList.map((option) => (
